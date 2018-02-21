4fad01fa311a37139514ed54265ced22f276f2f7
Corinne Krych <corinnekrych@gmail.com>
2018-02-21 11:31:04 +0100

    Revert (#10)
    * Revert "trying with relative path (#8)"
    
    This reverts commit 5dca0bdb8d5c94ba804d0cba9b429ed9daf1824c.
    
    * Revert "Gh.auth (#7)"
    
    This reverts commit 011b3b348d402e43f9fb342149d35bcbd77f671d.

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 'use strict';
 
 const unleash = require('unleash-server');
-const gitHubOAuth = require('./github-auth-hook');
-
-unleash.start({
-    adminAuthentication: 'custom',
-    preRouterHook: gitHubOAuth
-}).then(server => {
-    console.log(
-        `Unleash started on http://localhost:${server.app.get('port')}`
-    );
-});
+unleash.start({});
