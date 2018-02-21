046249656400f5108605655226f27dabc3c8520d
Corinne Krych <corinnekrych@gmail.com>
2018-02-21 11:58:39 +0100

    GH auth

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
 const unleash = require('unleash-server');
-unleash.start({});
+const gitHubOAuth = require('./github-auth-hook');
+
+unleash.start({
+    adminAuthentication: 'custom',
+    preRouterHook: gitHubOAuth
+}).then(server => {
+    console.log(
+        `Unleash started on http://localhost:${server.app.get('port')}`
+    );
+});
