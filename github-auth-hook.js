b12d4f802a3c9cdf40865039a731f940a935b8f0
Corinne Krych <corinnekrych@gmail.com>
2018-02-22 19:13:41 +0100

    handling error with more granularity for error message

--- github-auth-hook.js
+++ github-auth-hook.js
@@ -34,15 +34,19 @@ passport.use(
                 function(error, response, body) {
                     if (error) {
                         console.error('access to GH org failed:', error);
-                        return done(error);
+                        return done(null, false, { message: error });
                     } else if (response.statusCode != 200) {
                         console.error('access to GH org failed: ', response.statusCode, response.body);
-                        return done(null, false, { message: `unable to get the teams in the ${githubOrg} organization.` });
+                        return done(null, false, { message: `Unable to get the teams in the ${githubOrg} organization.` });
                     }
                     console.log('access to GH org done. Server responded with:', response.body);
                     let jsonBody = JSON.parse(response.body)
                     jsonBody.forEach(team => {
                         if (team.name == githubOrgTeam) {
+                            let user = new User({
+                                name: profile.displayName,
+                                email: profile.emails[0].value,
+                            });
                             console.log('found team URL: ', team.members_url);
                             let teamMemberURL = team.members_url.replace("{/member}", `/${profile.username}`);
                             console.log('using team URL: ', teamMemberURL);
@@ -56,19 +60,13 @@ passport.use(
                                 function(error, response, body) {
                                     if (error) {
                                         console.error('access to GH team failed:', error);
-                                        return done(error);
+                                        return done(null, false, { message: error});
                                     } else if (response.statusCode != 204) {
                                         console.error('access to GH team failed: ', response.statusCode, response.body);
-                                        return done(null, false, { message: 'User does not belong to the admin team.' });
+                                        return done(null, false, { message: `User does not belong to the ${githubOrgTeam} team.` });
                                     }
                                     // user belongs to the org/team
-                                    done(null,
-                                        new User({
-                                            name: profile.displayName,
-                                            email: profile.emails[0].value,
-                                            accessToken: accessToken,
-                                        })
-                                    );
+                                    done(null, user);
                                 }
                             );
                         }
@@ -80,8 +78,6 @@ passport.use(
         }
     )
 );
-
-
 
 function enableGitHubOAuth(app) {
 
@@ -99,19 +95,34 @@ function enableGitHubOAuth(app) {
 
     let context = process.env.TOGGLES_CONTEXT ? process.env.TOGGLES_CONTEXT : '';
 
-    app.get(
-        '/api/auth/callback',
-        passport.authorize('github', {
-            failureRedirect: `${context}/api/admin/error-login`,
-        }),
-        (req, res) => {
-            // redirect to toggles admin app.
-            res.redirect(`${context}/`);
+    // use custom callback http://www.passportjs.org/docs/authenticate/#custom-callback to better deal with error message
+    app.get('/api/auth/callback', (req, res, next) => {
+            passport.authenticate('github', (err, user, info) => {
+                let message = info.message;
+                if(!user) {
+                    req.session.error = message;
+                    return res.redirect(`${context}/#/features`);
+                } else {
+                    return res.redirect(`${context}/`);
+                }
+            })(req, res, next);
         });
-
+    
     app.use('/api/admin/', (req, res, next) => {
         console.log(`Calling /api/admin with req=${req} and res=${res}`)
-        if (req.user) {
+        if (req.session.error) {
+            // todo with 403 and AuthorizationRequired - once the ui supports it
+            return res
+                .status('401')
+                .json(
+                    new AuthenticationRequired({
+                        path: '/api/admin/login',
+                        type: 'none',
+                        message: req.session.error,
+                    })
+                )
+                .end();
+        } else if (req.user) {
             next();
         } else {
             // Instruct unleash-frontend to pop-up auth dialog
@@ -119,7 +130,7 @@ function enableGitHubOAuth(app) {
                 .status('401')
                 .json(
                     new AuthenticationRequired({
-                        path: 'api/admin/login',
+                        path: '/api/admin/login',
                         type: 'custom',
                         message: `You have to identify yourself in order to use Unleash. 
                         Click the button and follow the instructions.`,
