397176bccab2e8f59a43ac900defc26803f42421
Corinne Krych <corinnekrych@gmail.com>
2018-03-07 14:36:51 +0100

    chore(CI/CD): make sure to hook with job configuration

--- cico_build_deploy.sh
+++ cico_build_deploy.sh
@@ -38,14 +38,12 @@ function login() {
 /usr/sbin/setenforce 0
 
 # Get all the deps in
-
+yum install -y epel-release && yum install -y nodejs && yum clean all
 yum -y install \
    docker \
    make \
    git \
-   curl \
-   epel-release \
-   nodejs
+   curl
 
 # run build: lint, unit test
 echo 'Run nodejs lint and unit test'
